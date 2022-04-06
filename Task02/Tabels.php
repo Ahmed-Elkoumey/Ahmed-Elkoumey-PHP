811aaa6c076075722bb5f2b0165e50a5cfcdc516
Ahmed Elkoumey <ahmedelkoumey1997@gmail.com>
2022-04-07 00:27:09 +0200

    Second Task02

---
+++ Task02/Tabels.php
@@ -0,0 +1,108 @@
+<?php 
+
+// dynamic table
+// dynamic rows
+// dynamic columns
+// check if gender of user == m ==> male
+// check if gender of user == f ==> female
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'Ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],
+        
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'Mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'Menna',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+    ],  
+];
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+
+  <table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th class="col-2">ID</th>
+      <th class="col-2">User Name</th>
+      <th class="col-2">Gender</th>
+      <th class="col-2">Hobbies</th>
+      <th class="col-2">Activities</th>
+      
+    </tr>
+  </thead>
+  <?php  foreach ($users as  $userTab){   ?>
+
+  <tbody>
+    <tr>
+      <th class="col-2" ><?= $userTab->id?></th>
+      <td class="col-2"><?= $userTab->name?></td>
+      <td class="col-2"><?php foreach ($userTab->gender as  $gender) {echo ($gender=='m')?  'Male' : 'Female'; } ?></td>
+      <td class="col-2"><?php foreach ($userTab->hobbies as  $hobby){echo  " ( $hobby ) <br/>";} ?></td>
+      <td class="col-2"><?php foreach ($userTab->activities as $key => $active){echo  " $key -> ($active ) <br/>";} ?></td>
+    </tr>
+   
+  </tbody>
+  <?php }?>
+</table>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
